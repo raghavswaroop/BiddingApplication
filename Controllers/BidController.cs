9df1db3e0bcc1aba66c35cfd460b785b4b15a213
raghavswaroop@gmail.com <raghavswaroop@gmail.com>
2020-05-30 22:28:46 +0200

    intial commit

---
+++ Controllers/BidController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BiddingApplication.Controllers
+{
+    public class BidController : Controller
+    {
+        // GET: Bid
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+       
+        public ActionResult helloAngular()
+        {
+            return View();
+        }
+
+      
+     
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BiddingApplication.Models
+{
+    public class ProductModel
+    {
+        private int productname;
+
+        private int productID;
+
+
+        private String productDescription;
+
+
+        private int productBidValue;
+
+
+
+        public int ProductBidValue
+        {
+            get { return productBidValue; }
+            set { productBidValue = value; }
+        }
+
+
+
+
+        public String ProductDescription
+        {
+            get { return productDescription; }
+            set { productDescription = value; }
+        }
+
+
+
+
+        public int ProductName
+        {
+            get { return productname; }
+            set { productname = value; }
+        }
+
+
+
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+    }
+}
